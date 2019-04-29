9c929a02adde87de398740f53edc1e56ecda2dd6
TNguyen <trnguyen1510@gmail.com>
2019-04-28 18:25:14 -0700

    added navbar into sign up page

--- project-3/client/src/components/NavBar/index.js
+++ project-3/client/src/components/NavBar/index.js
@@ -4,18 +4,20 @@ import * as React from "react";
 import "../../assets/additionalcss/css/signup.css"
 import "../../assets/additionalcss/css/mbr-additional.css"
 import "../../assets/theme/css/style.css"
-import "../../assets/icons/icons.css"
+import "../../assets/web/assets/icons/icons.css"
 import "../../assets/tether/tether.min.css"
 import "../../assets/bootstrap/css/bootstrap.min.css"
 import "../../assets/bootstrap/css/bootstrap-grid.min.css"
 import "../../assets/dropdown/css/style.css"
 import "../../assets/animatecss/animate.min.css"
-import "../../assets/socicon/css/style.css"
+import "../../assets/socicon/css/styles.css"
 
-const NavBar = props => {
+
+
+const NavBar = (props) => {
 
     return (
-  
+
 <section className="menu cid-roBIQr2OFc" once="menu" id="menu1-0">
 
     <nav className="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
@@ -31,16 +33,17 @@ const NavBar = props => {
                 <div className="navbar-brand">
                     <span className="navbar-logo">
                         
-                             <img src="assets/images/owl-122x122.png" alt="" title="" style="height: 4.4rem;">
+                             <img src="assets/images/owl-122x122.png" alt="" title="" height="4.4rem"/>
                         
                     </span>
                     <span className="navbar-caption-wrap"><a class="navbar-caption text-success display-2" href="/">Affluent</a></span>
                 </div>
             </div>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
-                        <a className="nav-link link text-success display-4" href="/profile"><span class="mbri-cash mbr-iconfont mbr-iconfont-btn"></span>
-                            
+                <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                    <li class="nav-item">
+                        <a className="nav-link link text-success display-4" href="/profile">
+                        <span class="mbri-cash mbr-iconfont mbr-iconfont-btn"></span>  
                             Services
                         </a>
                     </li>
@@ -48,11 +51,25 @@ const NavBar = props => {
                         <a className="nav-link link text-success display-4" href="/contactus"><span class="mbri-hot-cup mbr-iconfont mbr-iconfont-btn"></span>
                             About Us
                         </a>
-                    </li></ul>
-                <div className="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-success-outline display-4" href="/signup">
+                    </li>
+                    
+                </ul>
+
+                <div className="navbar-buttons mbr-section-btn">
+                    <a class="btn btn-sm btn-success-outline display-4" href="/signup">
                         <span className="mbri-save mbr-iconfont mbr-iconfont-btn "></span>
                         Try It Now!
-                    </a></div>
+                    </a>
+                </div>
             </div>
-            </nav>
+        
+    </nav>
 </section>
+
+
+);
+}
+
+
+  
+export default NavBar; 
