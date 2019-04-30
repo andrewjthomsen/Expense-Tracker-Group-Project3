3da19d98f9eb00cfad722aa4f757f15c3e8c8ab5
andrew <andrewjthomsen@yahoo.com>
2019-04-30 15:30:54 -0700

    Wrote out CRUD operations in expenseController.js

--- project-3/controllers/expenseController.js
+++ project-3/controllers/expenseController.js
@@ -1,37 +1,51 @@
+// WROTE OUT CRUD OPERATIONS. NOT SURE IF COMPLETELY CORRECT NEED TO BE REVIEWED.
 const db = require("../models");
 
 // This info needs to be specific to the user
 // AKA: John Smith's expenses, not everyone's expenses
 
 module.exports = {
-    // Find all expenses BY USER
-    findAllExpenses: function(req, res) {
-        db.expenseTracker
-        // TO DO: fill in code for finding all expenses
-        .catch(err => res.status(422).json(err));
-    },
-    // Find a specific expense
-    findExpenseByID: function(req, res) {
-        db.expenseTracker
-        // TO DO: fill in code for finding a specific expense
-        .catch(err => res.status(422).json(err));
-    },
-    // Create a new expense
-    create: function(req, res) {
-        db.expenseTracker
-        // TO DO: fill in code for creating a new expense
-        .catch(err => res.status(422).json(err));
-    },
-    // Update a past expense
-    update: function(req, res) {
-        db.expenseTracker
-        // TO DO: fill in code for editing a past expense
-        .catch(err => res.status(422).json(err));
-    },
-    // Permanently delete expense
-    delete: function(req, res) {
-        db.expenseTracker
-        // TO DO: fill in code for deleting an expense
-        .catch(err => res.status(422).json(err));
-    }
+  // Find all expenses BY USER
+  findAllExpenses: function(req, res) {
+    db.expenseTracker
+      // TO DO: fill in code for finding all expenses
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel)) // Need to connect to mongodb
+      .catch(err => res.status(422).json(err));
+  },
+  // Find a specific expense
+  findExpenseByID: function(req, res) {
+    db.expenseTracker
+      // TO DO: fill in code for finding a specific expense
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // Create a new expense
+  create: function(req, res) {
+    db.expenseTracker;
+    // TO DO: fill in code for creating a new expense
+    create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // Update a past expense
+  update: function(req, res) {
+    db.expenseTracker
+      // TO DO: fill in code for editing a past expense
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // Permanently delete expense
+  remove: function(req, res) {
+    // delete in mongo db
+    db.expenseTracker
+      // TO DO: fill in code for deleting an expense
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 };
