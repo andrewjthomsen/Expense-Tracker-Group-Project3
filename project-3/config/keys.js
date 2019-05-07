4f79aa89d81959a2cd16f2bbb77820c3c7956a20
Jamferg <jamaalfergusontrust@jamaals-MacBook-Pro.local>
2019-05-06 18:52:35 -0700

    login features

---
+++ project-3/config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoURI: "mongodb://localhost/ExpenseTracker"
+  };
+
+  module.exports = {
+    mongoURI: "mongodb://localhost/ExpenseTracker",
+    secretOrKey: "secret"
+  };
+
