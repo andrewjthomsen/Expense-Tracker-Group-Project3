cf4954be700c89d7a68bc565694d898be1034503
Rachel Harris <rjhart14@gmail.com>
2019-05-07 21:40:17 -0700

    Calculator updated

--- project-3/client/src/components/Calc/calc.js
+++ project-3/client/src/components/Calc/calc.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
-import Input from "./Buttons/input";
-import { CalcButton, ClearButton } from "./Buttons/buttons";
+import Input from "./Input/input";
+import { CalcButton, ClearButton, BackButton } from "./Buttons/buttons";
 import * as math from "mathjs";
 
 class Calc extends React.Component {
@@ -21,38 +21,61 @@ class Calc extends React.Component {
     this.setState({ input: math.eval(this.state.input) });
   };
 
+  handleBack = () => {
+    if (this.state.input !== "") {
+      const deletedNumber = this.state.input.slice(
+        0,
+        this.state.input.length - 1
+      );
+      this.setState({ input: deletedNumber });
+    }
+  };
+
   render(props) {
     return (
       <div className="calc-wrapper">
         <Input input={this.state.input} />
-        <div className="row">
+        <div className="calc-row">
+          <div className="calc-col-2">
+            <ClearButton handleClear={() => this.setState({ input: "" })}>
+              Clear
+            </ClearButton>
+          </div>
+          <div className="calc-col-1">
+            <BackButton handleClick={() => this.handleBack()}>&#60;</BackButton>
+          </div>
+          <div className="calc-col-1">
+            <CalcButton handleClick={this.addToInput}>/</CalcButton>
+          </div>
+        </div>
+        <div className="calc-row">
           <CalcButton handleClick={this.addToInput}>7</CalcButton>
           <CalcButton handleClick={this.addToInput}>8</CalcButton>
           <CalcButton handleClick={this.addToInput}>9</CalcButton>
-          <CalcButton handleClick={this.addToInput}>/</CalcButton>
+          <CalcButton handleClick={this.addToInput}>*</CalcButton>
         </div>
-        <div className="row">
+        <div className="calc-row">
           <CalcButton handleClick={this.addToInput}>4</CalcButton>
           <CalcButton handleClick={this.addToInput}>5</CalcButton>
           <CalcButton handleClick={this.addToInput}>6</CalcButton>
-          <CalcButton handleClick={this.addToInput}>*</CalcButton>
+          <CalcButton handleClick={this.addToInput}>-</CalcButton>
         </div>
-        <div className="row">
+        <div className="calc-row">
           <CalcButton handleClick={this.addToInput}>1</CalcButton>
           <CalcButton handleClick={this.addToInput}>2</CalcButton>
           <CalcButton handleClick={this.addToInput}>3</CalcButton>
           <CalcButton handleClick={this.addToInput}>+</CalcButton>
         </div>
-        <div className="row">
-          <CalcButton handleClick={this.addToInput}>.</CalcButton>
-          <CalcButton handleClick={this.addToInput}>0</CalcButton>
-          <CalcButton handleClick={() => this.handleEqual()}>=</CalcButton>
-          <CalcButton handleClick={this.addToInput}>-</CalcButton>
-        </div>
-        <div className="row">
-          <ClearButton handleClear={() => this.setState({ input: "" })}>
-            Clear
-          </ClearButton>
+        <div className="calc-row">
+          <div className="calc-col-1">
+            <CalcButton handleClick={this.addToInput}>.</CalcButton>
+          </div>
+          <div className="calc-col-1">
+            <CalcButton handleClick={this.addToInput}>0</CalcButton>
+          </div>
+          <div className="calc-col-2">
+            <CalcButton handleClick={() => this.handleEqual()}>=</CalcButton>
+          </div>
         </div>
       </div>
     );
