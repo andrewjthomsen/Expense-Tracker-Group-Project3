4420385c953cce741a63c4b9ed1a861e6aa91153
TNguyen <trnguyen1510@gmail.com>
2019-05-13 23:25:42 -0700

    I added homepage and reroute the signin and sign up page

--- project-3/client/src/index.js
+++ project-3/client/src/index.js
@@ -5,12 +5,11 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import SignUp from "./views/SignUp/signup";
 import SignIn from "./views/SignIn/SignIn";
 import expenseForm from "../src/components/Expense-Form/expenseForm";
-
+import HomePage from "layouts/HomePage.jsx"
 import store from "./store";
 import Admin from "layouts/Admin.jsx";
-import RTL from "layouts/RTL.jsx";
 import { Provider } from "react-redux";
-
+import "assets/scss/material-kit-react.scss?v=1.4.0";
 import "assets/css/material-dashboard-react.css?v=1.6.0";
 
 const hist = createBrowserHistory();
@@ -19,12 +18,11 @@ ReactDOM.render(
   <Provider store={store}>
     <Router history={hist}>
       <Switch>
+        <Route path="/" component={HomePage} />
         <Route path="/admin" component={Admin} />
-        <Route path="/rtl" component={RTL} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/expenseForm" component={expenseForm} />
-        <Redirect from="/" to="/admin/dashboard" />
+        <Route exact path="/admin/expenseForm" component={expenseForm} />
       </Switch>
     </Router>
   </Provider>,
