e0e0ca9724fcbf56133b0f2219733517759075d7
andrew <andrewjthomsen@yahoo.com>
2019-05-18 11:55:32 -0700

    Finished working on dashboard expensecard

--- project-3/client/src/components/ExpensesCard/ExpenseCardBody.jsx
+++ project-3/client/src/components/ExpensesCard/ExpenseCardBody.jsx
@@ -13,100 +13,76 @@ import cardBodyStyle from "assets/jss/material-dashboard-react/components/cardBo
 // BRING IN AXIOS
 import axios from "axios";
 import "./style/expenseCardStyle.css";
-function CardBody({ ...props }) {
-  const { classes, className, children, plain, profile, ...rest } = props;
-  const cardBodyClasses = classNames({
-    [classes.cardBody]: true,
-    [classes.cardBodyPlain]: plain,
-    [classes.cardBodyProfile]: profile,
-    [className]: className !== undefined
-  });
-  // SETTING STATE ON CARDBODY
-  const [data, setData] = useState({ expenses: [] });
-  // console.log("expenses are:", data.expenses);
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios("http://localhost:5000/api/expenses");
+class CardBody extends React.Component {
+  constructor(props) {
+    super(props);
 
-      setData(result.data);
-      // console.log(result);
-      // console.log(result.data);
-      // console.log(result.data[0]);
-      const arrayData = Object.keys(result.data).map(function(key) {
-        return [Object(key), result.data[key]];
-      });
-      // console.log returns array data function
-      // console.log("arrayData:", arrayData[0][1].payee);
-      // console.log("arrayData:", arrayData[1][1].amount);
-      console.log("arrayData:", arrayData[0][1].category);
-      // console.log("arrayData:", arrayData[1][1].comment);
-      // NEED TO WRITE FORLOOP TO LOOP THROUGH FIRST FIVE MOST RECENT EXPENSES ADDED
-      // NEED ANOTHER LOOP THAT PUSHES RESULTS INTO AN ARRAY
-      // WILL HAVE AN ARRAY OF 5 ARRAYS
-      // WHAT TABLE DATA WILL BE EQUAL TO
-
-      // var getSet = ""; // Get Skills from the Array
-      // var getSetIcon = data.sets[value].set[0]; // Get Skills Icon
-      // var getSetTitle = data.sets[value].set[1]; // Get Equip's Title
-
-      // for(var val = 2; val <= data.sets[value].set.length - 1; val++) {
-      //     getSet += data.sets[value].set[val]; // Get Skills info
-      // }
-      // for (var j = 1; j <= arrayData.length; j++) {
-      //   console.log(
-      //     arrayData[i][j].payee + " " + arrayData[i + 1][j + 1].amount
-      //   );
-      // }
+    this.state = {
+      data: {}
     };
-    fetchData();
-    // const tableInfo = data.expenses;
-    // console.log("tableInfo is:", tableInfo);
-  }, []);
+  }
 
-  // item => (key = item.objectID
+  async componentDidMount() {
+    const result = await axios("http://localhost:5000/api/expenses");
 
-  // recentExpenses = () => {
-  //   const result = axios("http://localhost:5000/api/expenses");
+    let data = result.data;
+    console.log(data);
+    data.sort((a, b) => {
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+    });
+    console.log(data);
 
-  //   setData(result.data);
-  //   // console.log(result);
-  //   // console.log(result.data);
-  //   // console.log(result.data[0]);
-  //   const arrayData = Object.keys(result.data).map(function(key) {
-  //     return [Object(key), result.data[key]];
-  //   });
+    this.setState({ data: data });
+    // console.log(result);
+    // console.log(result.data);
+    // console.log(result.data[0]);
+    const arrayData = Object.keys(result.data).map(function(key) {
+      return [Object(key), result.data[key]];
+    });
+    // console.log returns array data function
+    // console.log("arrayData:", arrayData[0][1].payee);
+    // console.log("arrayData:", arrayData[1][1].amount);
+    console.log("arrayData:", arrayData[0][1].category);
+    // console.log("arrayData:", arrayData[1][1].comment);
+    // NEED TO WRITE FORLOOP TO LOOP THROUGH FIRST FIVE MOST RECENT EXPENSES ADDED
+    // NEED ANOTHER LOOP THAT PUSHES RESULTS INTO AN ARRAY
+    // WILL HAVE AN ARRAY OF 5 ARRAYS
+    // WHAT TABLE DATA WILL BE EQUAL TO
+  }
 
-  //   let table = [];
-  //   let children = [];
-
-  //   for (var i = 0; i <= arrayData.length - 1; i++) {
-  //  {children.push(<td>{arrayData[i][1].payee}</td>);
-  //     table.push(<tr>{children}</tr>);
-  //   }
-  // };
-  return (
-    <div className={cardBodyClasses} {...rest}>
-      {children}
-      <div>
-        {/* <Balance className="balanceStyle"/> */}
-        {/* <Table
-          tableHeaderColor="warning"
-          tableHead={["ID", "Name", "Salary", "Country"]}
-          tableData={[
-            ["1", "Dakota Rice", "$36,738", "Niger"],
-            ["2", "Minerva Hooper", "$23,789", "Curaçao"],
-            ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
-            ["4", "Philip Chaney", "$38,735", "Korea, South"]
-          ]}
-        /> */}
-        {/* <Table
-          tableHeaderColor="warning"
-          tableHead={["payee", "amount", "category", "comment"]}
-          // tableData={this.state}
-        /> */}
+  render() {
+    const { classes, className, children, plain, profile, ...rest } = this.props;
+    const cardBodyClasses = classNames({
+      [classes.cardBody]: true,
+      [classes.cardBodyPlain]: plain,
+      [classes.cardBodyProfile]: profile,
+      [className]: className !== undefined
+    });
+    let tableData = [];
+    for (var i = 0; i < this.state.data.length && i < 5; ++i) {
+      let expense = this.state.data[i];
+      let row = [ expense.payee, expense.amount, expense.category, expense.comment ];
+      tableData.push(row);
+    }
+    return (
+      <div className={cardBodyClasses} {...rest}>
+        {children}
+        <div>
+          {/* <Balance className="balanceStyle"/> */}
+          <Table
+            tableHeaderColor="warning"
+            tableHead={["Payee", "Amount", "Category", "Comment"]}
+            tableData={tableData}
+          />
+          {/* <Table
+            tableHeaderColor="warning"
+            tableHead={["payee", "amount", "category", "comment"]}
+            // tableData={this.state}
+          /> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 CardBody.propTypes = {
@@ -161,3 +137,41 @@ export default withStyles(cardBodyStyle)(CardBody);
 // Need to parse data in array of strings
 // NEED TO GET DATA IN CONSOLE TO LOOK LIKE EXAMPLE BELOW
 // EXAMPLE COMES FROM DASHBOARD.JSX EXPENSES CARD AT BOTTOM OF CODE PAGE
+
+
+
+        // var getSet = ""; // Get Skills from the Array
+        // var getSetIcon = data.sets[value].set[0]; // Get Skills Icon
+        // var getSetTitle = data.sets[value].set[1]; // Get Equip's Title
+
+        // for(var val = 2; val <= data.sets[value].set.length - 1; val++) {
+        //     getSet += data.sets[value].set[val]; // Get Skills info
+        // }
+        // for (var j = 1; j <= arrayData.length; j++) {
+        //   console.log(
+        //     arrayData[i][j].payee + " " + arrayData[i + 1][j + 1].amount
+        //   );
+        // }
+
+        
+    // item => (key = item.objectID
+
+    // recentExpenses = () => {
+    //   const result = axios("http://localhost:5000/api/expenses");
+
+    //   setData(result.data);
+    //   // console.log(result);
+    //   // console.log(result.data);
+    //   // console.log(result.data[0]);
+    //   const arrayData = Object.keys(result.data).map(function(key) {
+    //     return [Object(key), result.data[key]];
+    //   });
+
+    //   let table = [];
+    //   let children = [];
+
+    //   for (var i = 0; i <= arrayData.length - 1; i++) {
+    //  {children.push(<td>{arrayData[i][1].payee}</td>);
+    //     table.push(<tr>{children}</tr>);
+    //   }
+    // };
