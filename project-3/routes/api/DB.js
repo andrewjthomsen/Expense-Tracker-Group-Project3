3bc232c11628fed5bef385c44c29fac55036cac5
andrew <andrewjthomsen@yahoo.com>
2019-05-20 16:55:33 -0700

    Deploying to heroku

--- project-3/routes/api/DB.js
+++ project-3/routes/api/DB.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/expenses'
+    // DB: 'mongodb://localhost:27017/expenses'
+    DB: '//@ds157956.mlab.com:57956/heroku_p53pnr84'
 }
--- project-3/server.js
+++ project-3/server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const path = require("path")
 const mongoose = require('mongoose');
 const config = require('./routes/api/DB');
 const expenseRoute = require('./routes/api/expenseAPI');
-
+const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/expenses'
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+mongoose.connect(db, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
 );
-
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/api', expenseRoute);
 
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
 });
