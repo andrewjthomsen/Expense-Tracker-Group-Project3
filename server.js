3ece107ab2c419c40ab1af4fb8555097b487895d
andrew <andrewjthomsen@yahoo.com>
2019-05-21 15:39:39 -0700

    Fixing issues to connect to heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const path = require("path")
 const mongoose = require('mongoose');
+require("dotenv").config();
 const config = require('./routes/api/DB');
 const expenseRoute = require('./routes/api/expenseAPI');
 const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/expenses'
