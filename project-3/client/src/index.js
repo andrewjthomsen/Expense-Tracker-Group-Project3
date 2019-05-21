ff1d7f197dff50a6a78f65332516186afd76d35e
Jamferg <jamaalfergusontrust@jamaals-MacBook-Pro.local>
2019-05-20 18:53:17 -0700

    heroku push

--- project-3/client/src/index.js
+++ project-3/client/src/index.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-<<<<<<< HEAD
-import SignUp from "./views/SignUp/signup";
-import SignIn from "./views/SignIn/SignIn";
-
-
-=======
 import SignUp from "./layouts/signup";
 import SignIn from "./layouts/signin";
 import expenseForm from "../src/components/Expense-Form/expenseForm";
 import HomePage from "layouts/HomePage.jsx"
->>>>>>> 96e01784d16c3b801f95b2acbfb2f7e43fc69f16
 import store from "./store";
 import Admin from "layouts/Admin.jsx";
 import { Provider } from "react-redux";
@@ -22,19 +15,6 @@ import "assets/css/material-dashboard-react.css?v=1.6.0";
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-<<<<<<< HEAD
-  <Provider store ={store}>
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/rtl" component={RTL} />
-      <Route exact path="/signin" component={SignIn} />
-      <Route exact path="/signup" component={SignUp} />
- 
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </Router>
-=======
   <Provider store={store}>
     <Router history={hist}>
       <Switch>
@@ -46,7 +26,6 @@ ReactDOM.render(
         <Route exact path="/admin/expenseForm" component={expenseForm} />
       </Switch>
     </Router>
->>>>>>> 96e01784d16c3b801f95b2acbfb2f7e43fc69f16
   </Provider>,
   document.getElementById("root")
 );
--- project-3/server.js
+++ project-3/server.js
@@ -1,28 +1,17 @@
-
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const userAPI = require("./routes/api/userAPI");
-<<<<<<< HEAD
-const app = express();
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const cors = require("cors");
-// Loads in Mongoose database
-const config = require("./routes/api/DB");
-// BRING IN ROUTE FOR EXPENSE FORM
-const expenseRoute = require("./routes/api/expenseAPI");
-=======
 const passport = require("passport");
 const path = require("path");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const mongoose = require("mongoose");
 const expenseRoute = require("./routes/api/expenseAPI");
 
 // // DB Config
 const db = require("./config/keys");
-const db2 =require("./routes/api/DB");
+
 mongoose.Promise = global.Promise;
 mongoose.connect(db.mongoURI, { useNewUrlParser: true }).then(
   () => {
@@ -45,6 +34,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 // Passport middleware
 app.use(passport.initialize());
 
@@ -53,24 +43,8 @@ require("./config/passport")(passport);
 
 // routes
 app.use("/api", expenseRoute);
-app.use("/user", userAPI);
->>>>>>> 96e01784d16c3b801f95b2acbfb2f7e43fc69f16
+app.use("/api/userAPI", userAPI);
 
-
-// Bring in Mongoose
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
-
-// Bring in CORS
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-
-const expenseAPI = require("./routes/api/expenseAPI");
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -78,53 +52,6 @@ app.use(
   })
 );
 
-<<<<<<< HEAD
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// DB Config
-const db = require("./config/keys").mongoURI;
-
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-
-// // Define API routes here
-app.use(passport.initialize());
-app.use("/api", userAPI)
-const PORT = process.env.PORT || 5000; 
-
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
-
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.use('/business', expenseRoute);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.use(routes);
-
-app.listen(PORT, function(){
-  console.log('Server is running on Port:',PORT);
-});
-=======
 app.listen(PORT, function() {
   console.log("Server is running on Port:", PORT);
 });
->>>>>>> 96e01784d16c3b801f95b2acbfb2f7e43fc69f16
